c91a41640833e171f8913331889de36bd412c0ff
macmay91 <macmay91@gmail.com>
2024-10-11 10:01:59 -0400

    Finished project

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def main():
+    path = "books/frankenstein.txt"
     with open("books/frankenstein.txt") as f:
         text = f.read()
         
     numbers = word_count(text)
-    print(numbers)
     characters = character_count(text)
+    character_list = ordered_list(characters)
+
+    print_report(path, numbers, character_list)
+
+
+
 
 def word_count(text_file):
         words = text_file.split()
@@ -23,7 +29,7 @@ def character_count(text_file):
     for letter in alphabet:
         d[letter] = 0
     
-    print(d)
+    
 
     for letter in text_file:
          try:
@@ -32,10 +38,31 @@ def character_count(text_file):
          except:
               pass
     
-    print(d)
+    return d
+    
+    
+def ordered_list(character_count):
+     character_list = []
 
+     for x, y in character_count.items():
+         character_list.append({"letter": x, "num": y})
+    
+     character_list.sort(reverse=True, key=sort_on)
+    
+     return character_list
 
+def print_report(book_path, words, character_list):
+     
+     print(f"--- Begin report of {book_path} ---")
+     print(f"{words} words found in the document")
+     print()
 
+     for count in character_list:
+          print(f"The '{count['letter']}' character was found {count['num']} times")
+     print("--- End report ---")
+
+def sort_on(dict):
+     return dict["num"]
 
 
 if __name__ == "__main__":
